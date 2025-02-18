ab1810239529d6ba8238d2da879a59ce242e3aae
mutalipovv <mutalipovmarufjon221@gmail.com>
2025-02-18 22:33:54 +0500

    end

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+const darcMode = document.querySelector(".d");
 
+darcMode.addEventListener("click", function () {
+  document.body.classList.toggle("dark-mode");
+
+  if (document.body.classList.contains("dark-mode")) {
+    darcMode.innerHTML = `
+        <img id="light-mode" src="./images/Icon (1).svg" width="19" alt="">`;
+    document.body.classList.add(".dark-mode");
+  } else {
+    darcMode.innerHTML = `
+        <img id="dark-mode" src="./images/Settings Icon.svg" width="19" alt="">`;
+  }
+});
